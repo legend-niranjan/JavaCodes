bb400298470a8437fa5e8895a23e91dea8d939b3
legend-niranjan <niranjansurywanshi318@gmail.com>
2026-04-20 17:30:07 +0530

    first commit

---
+++ stringOperations.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class stringOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int size=sc.nextInt();
+        String imp[]=new String[size];
+        int totalLength=0;
+        for(int i=0;i<size;i++){
+            imp[i]=sc.next();
+            totalLength+=imp[i].length();
+        }
+        System.out.println(totalLength);
+        for(int j=0;j<size;j++){
+            System.out.print(imp[j]+",");
+        }
+    }
+}
+
